e160fa4b822e909b2af9001970e7b0d8920cd06a
Josh Leath <joshua.leath@icloud.com>
2022-08-17 19:32:22 -0400

    add height field and helper methods

--- src/canvas.rs
+++ src/canvas.rs
@@ -3,6 +3,7 @@ use crate::color::Color;
 pub struct Canvas {
     buffer: Vec<Color>,
     width: usize,
+    height: usize,
 }
 
 impl Canvas {
@@ -10,6 +11,7 @@ impl Canvas {
         Canvas {
             buffer: vec![Color::new(0.0, 0.0, 0.0); width * height],
             width,
+            height,
         }
     }
 
@@ -19,5 +21,17 @@ impl Canvas {
 
     pub fn write_pixel(&mut self, x: usize, y: usize, c: Color) {
         self.buffer[(y * self.width) + x] = c;
+    }
+
+    pub fn width(&self) -> usize {
+        self.width
+    }
+
+    pub fn height(&self) -> usize {
+        self.height
+    }
+
+    pub fn pixels(&self) -> &Vec<Color> {
+        &(self.buffer)
     }
 }
