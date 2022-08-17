3f7043c289b3367314505d31a696aab745dbb8ca
Josh Leath <joshua.leath@icloud.com>
2022-08-17 13:24:54 -0400

    add operator traits for addition, subtraction, and negation

--- src/tuple.rs
+++ src/tuple.rs
@@ -1,3 +1,5 @@
+use std::ops;
+
 fn float_near_equal(a: f64, b: f64) -> bool {
     (a - b).abs() < f64::EPSILON
 }
@@ -41,6 +43,54 @@ impl Tuple {
     }
 }
 
+impl ops::Add for Tuple {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+            w: self.w + other.w,
+        }
+    }
+}
+
+impl ops::AddAssign for Tuple {
+    fn add_assign(&mut self, rhs: Self) {
+        *self = *self + rhs;
+    }
+}
+
+impl ops::Sub for Tuple {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self {
+        Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+            w: self.w - other.w,
+        }
+    }
+}
+
+impl ops::SubAssign for Tuple {
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = *self - rhs;
+    }
+}
+
+impl ops::Neg for Tuple {
+    type Output = Self;
+    fn neg(self) -> Self {
+        Self {
+            x: -(self.x),
+            y: -(self.y),
+            z: -(self.z),
+            w: -(self.w),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -70,5 +120,50 @@ mod tests {
     fn vector_init() {
         let vector = Tuple::vector(4.0, -4.0, 3.0);
         assert_eq!(vector, Tuple::new(4.0, -4.0, 3.0, 0.0));
+    }
+
+    #[test]
+    fn addition() {
+        // add vector to point produces point
+        let mut a1 = Tuple::point(3.0, -2.0, 5.0);
+        let a2 = Tuple::vector(-2.0, 3.0, 1.0);
+        assert_eq!(a1 + a2, Tuple::point(1.0, 1.0, 6.0));
+
+        // AddAssign trait
+        a1 += a2;
+        assert_eq!(a1, Tuple::point(1.0, 1.0, 6.0));
+    }
+
+    #[test]
+    fn subtraction() {
+        // subtract point from point produces vector
+        let mut a1 = Tuple::point(3.0, 2.0, 1.0);
+        let mut a2 = Tuple::point(5.0, 6.0, 7.0);
+        assert_eq!(a1 - a2, Tuple::vector(-2.0, -4.0, -6.0));
+
+        // SubAssign trait
+        a1 -= a2;
+        assert_eq!(a1, Tuple::vector(-2.0, -4.0, -6.0));
+
+        // subtract vector from point produces point
+        a1 = Tuple::point(3.0, 2.0, 1.0);
+        a2 = Tuple::vector(5.0, 6.0, 7.0);
+        assert_eq!(a1 - a2, Tuple::point(-2.0, -4.0, -6.0));
+
+        // subtract vector from vector produces vector
+        a1 = Tuple::vector(3.0, 2.0, 1.0);
+        a2 = Tuple::vector(5.0, 6.0, 7.0);
+        assert_eq!(a1 - a2, Tuple::vector(-2.0, -4.0, -6.0));
+
+        // subtracting from zero vector
+        let zero = Tuple::vector(0.0, 0.0, 0.0);
+        let v = Tuple::vector(1.0, -2.0, 3.0);
+        assert_eq!(zero - v, Tuple::vector(-1.0, 2.0, -3.0));
+    }
+
+    #[test]
+    fn negation() {
+        let a = Tuple::new(1.0, -2.0, 3.0, -4.0);
+        assert_eq!(-a, Tuple::new(-1.0, 2.0, -3.0, 4.0));
     }
 }
