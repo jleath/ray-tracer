cf5093ed420ee38e9521e2315d39f9d9c80a495e
Joshua Leath <lawnfurniture@icloud.com>
2022-08-19 14:47:03 -0400

    basic ray tracing with no lighting

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,40 @@
 use ray_tracer::canvas::Canvas;
 use ray_tracer::color::Color;
+use ray_tracer::intersection::IntersectionList;
 use ray_tracer::ppm_printer::PpmPrinter;
-use ray_tracer::transform::Transform;
+use ray_tracer::ray::Ray;
+use ray_tracer::sphere::Sphere;
 use ray_tracer::tuple::Tuple;
-use std::f64::consts::PI;
-
-const CANVAS_WIDTH: usize = 500;
-const CANVAS_HEIGHT: usize = 500;
 
 fn main() {
-    let mut canvas = Canvas::new(CANVAS_WIDTH, CANVAS_HEIGHT);
-    let point_color = Color::new(1.0, 1.0, 1.0);
-    let twelve_point = Tuple::point(0.0, 0.0, 1.0);
-    let radius = CANVAS_WIDTH as f64 * (3.0 / 8.0);
-    let mut hour_points = vec![];
+    let ray_origin = Tuple::point(0.0, 0.0, -5.0);
+    let wall_z = 10.0;
+    let wall_size = 7.0;
+    let canvas_pixels = 300.0;
+    let pixel_size = wall_size / canvas_pixels;
+    let half = wall_size / 2.0;
 
-    for hour in 1..=12 {
-        let hour_point = Transform::new()
-            .rotate_y(hour as f64 * (PI / 6.0))
-            .transform(&twelve_point);
-        hour_points.push(hour_point);
+    let mut canvas = Canvas::new(canvas_pixels as usize, canvas_pixels as usize);
+    let point_color = Color::new(1.0, 0.0, 0.0);
+    let mut shape = Sphere::new();
+    shape.transform = shape
+        .transform
+        .shear(1.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+        .scale(0.5, 1.0, 1.0);
+
+    for y in 0..canvas_pixels as usize {
+        let world_y = half - pixel_size * y as f64;
+
+        for x in 0..canvas_pixels as usize {
+            let world_x = -half + pixel_size * x as f64;
+            let position = Tuple::point(world_x as f64, world_y as f64, wall_z);
+            let r = Ray::new(ray_origin, (position - ray_origin).normalize());
+            let mut xs = IntersectionList::new(&shape.intersect(&r));
+            if xs.hit().is_some() {
+                canvas.write_pixel(x as usize, y as usize, point_color);
+            }
+        }
     }
 
-    for point in hour_points {
-        let x = (point.x * radius) + (CANVAS_WIDTH as f64 / 2.0);
-        let z = (point.z * radius) + (CANVAS_HEIGHT as f64 / 2.0);
-        canvas.write_pixel(x as usize, z as usize, point_color);
-    }
-
-    PpmPrinter::dump_to_file(&canvas, "clock.ppm").unwrap();
+    PpmPrinter::dump_to_file(&canvas, "sphere.ppm").unwrap();
 }
