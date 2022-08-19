cf7bb68819fb39743cb616906bc7a52a64fda99f
Joshua Leath <lawnfurniture@icloud.com>
2022-08-19 16:28:59 -0400

    rendering a sphere with lighting

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use ray_tracer::canvas::Canvas;
 use ray_tracer::color::Color;
 use ray_tracer::intersection::IntersectionList;
+use ray_tracer::point_light::PointLight;
 use ray_tracer::ppm_printer::PpmPrinter;
 use ray_tracer::ray::Ray;
 use ray_tracer::sphere::Sphere;
@@ -15,12 +16,13 @@ fn main() {
     let half = wall_size / 2.0;
 
     let mut canvas = Canvas::new(canvas_pixels as usize, canvas_pixels as usize);
-    let point_color = Color::new(1.0, 0.0, 0.0);
     let mut shape = Sphere::new();
-    shape.transform = shape
-        .transform
-        .shear(1.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-        .scale(0.5, 1.0, 1.0);
+    shape.material.color = Color::new(1.0, 0.2, 1.0);
+    let light_source = PointLight::new(Tuple::point(-10.0, 10.0, -10.0), Color::new(1.0, 1.0, 1.0));
+    // shape.transform = shape
+    //     .transform
+    //     .shear(1.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+    //     .scale(0.5, 1.0, 1.0);
 
     for y in 0..canvas_pixels as usize {
         let world_y = half - pixel_size * y as f64;
@@ -30,8 +32,15 @@ fn main() {
             let position = Tuple::point(world_x as f64, world_y as f64, wall_z);
             let r = Ray::new(ray_origin, (position - ray_origin).normalize());
             let mut xs = IntersectionList::new(&shape.intersect(&r));
-            if xs.hit().is_some() {
-                canvas.write_pixel(x as usize, y as usize, point_color);
+            if let Some(hit) = xs.hit() {
+                let point = r.position(hit.t);
+                let normal = hit.object.normal_at(point);
+                let eye = -(r.direction);
+                let color = hit
+                    .object
+                    .material
+                    .lighting(&light_source, point, eye, normal);
+                canvas.write_pixel(x as usize, y as usize, color);
             }
         }
     }
